0fc2fdc7b8eca779c41fa873e1d53ea35588ae33
Илья <realmozg@gmail.com>
2020-09-02 23:05:45 +0300

    test script parse

--- parse_v_2.py
+++ parse_v_2.py
@@ -2,10 +2,13 @@ from bs4 import BeautifulSoup
 import os
 import html.parser
 import sys
+import requests
+import re
 
 folder = 'C:\\Users\\demom\\Desktop\\Parse_site\\'
 source_file = folder+'site.html'
 file_log = folder+'log.txt'
+site_for_parse = 'https://ca.kontur.ru/about/certificates'
 
 def create_folder(folder):
     if os.path.exists(folder):
@@ -21,6 +24,7 @@ def create_folder(folder):
         else:
             print("Рабочий каталог %s создан"%dirs)
             return 1
+
 status = create_folder(folder)
 if status==0:
     print("Выход из программы")
@@ -28,6 +32,24 @@ if status==0:
 else:
     print ("Продолжаем работу")
     os.chdir(folder)
+
+
+def log_file (text, file_name):
+    if os.path.isfile(file_name):
+        with open(file_name, 'a', encoding='UTF-8') as file_name:
+            file_name.write(text+'\n')
+    else:
+        with open(file_name, 'w', encoding='UTF-8') as file_name:    
+            file_name.write(text+'\n')
+
+resp = requests.get(site_for_parse).text
+soup = BeautifulSoup(resp, "html.parser")
+result = soup.findAll(id=re.compile('^caCertifcicatesGroupId'))
+
+for i in result:
+    print(str(i.td.text)+"\n\n")
+    print(str(i.td.a.get('href'))+"\n\n")
+
 
 # openfile = open(file, "r", encoding="UTF-8")
 # file_log = open(file_log, "w", encoding="UTF-8")
