044fd3b6715b81e6bc4d937c87f0dfa17303f7ee
Мусатов Илья <realmozg@gmail.com>
2020-09-09 22:20:25 +0300

    error download file function

--- parse_v_2.py
+++ parse_v_2.py
@@ -5,34 +5,22 @@ import sys
 import requests
 import re
 
-folder = 'C:\\Users\\demom\\Desktop\\Parse_site\\'
-source_file = folder+'site.html'
-file_log = folder+'log.txt'
+folder = 'C:\\Users\\demom\\OneDrive\\Рабочий стол\\Новая папка'
 site_for_parse = 'https://ca.kontur.ru/about/certificates'
+
+################### Блок с функциями ######################
 
 def create_folder(folder):
     if os.path.exists(folder):
-        return 1
+        return os.path.dirname(folder)
     else:
         try:
-            new_dir = os.makedirs(folder)
-            dirs = os.path.dirname(folder)
+            os.makedirs(folder)
+            print("Каталог создан")
+            return os.path.dirname(folder)
         except OSError:
             print("Невозможно создать директорию")
             return 0
-        else:
-            print("Рабочий каталог %s создан"%dirs)
-            return 1
-
-os.chdir(folder)
-main_folder  = create_folder(folder)
-if main_folder==0:
-    print("Выход из программы")
-    sys.exit()
-else:
-    print ("Продолжаем работу")
-    os.chdir(folder)
-
 
 def log_file (text, file_name):
     if os.path.isfile(file_name):
@@ -42,35 +30,52 @@ def log_file (text, file_name):
         with open(file_name, 'w', encoding='UTF-8') as file_name:    
             file_name.write(text+'\n')
 
-def download_file(url, name, folder):
+def download_file(url, name):
     req = requests.get(url)
     file_data = req.content
-    with open(folder + name, 'wb') as f:
+    with open(name, 'wb') as f:
         f.write(file_data)
 
-# Написать функцию для сохранению файлов
+ #############################################
 
+main_folder  = create_folder(folder)
+os.chdir(folder)
+
+if main_folder==0:
+    print("Выход из программы")
+    sys.exit()
+else:
+    print ("Продолжаем работу")
+    os.chdir(folder)
 
 resp = requests.get(site_for_parse).text
 soup = BeautifulSoup(resp, "html.parser")
 result = soup.findAll('div', id=re.compile('^caCertifcicatesGroupId'))
 
+
+
 for i in result:
     result2 = i.findAll('td')
-
     for k in result2:
         if not re.search('Скачать', k.text):
             print(k.text)
             log_file(k.text, 'link.txt')
-            new_folder = k.text # использовать переменную для формирования записи файлов
-            create_folder(k.text)
+            # new_folder = k.text # использовать переменную для формирования записи файлов
+            new_folder = create_folder(k.text)
         else:
-            if k.a.get('href').startswith('/Files') or k.a.get('href').startswith('/cdp/'):
+            if  k.a.get('href').startswith('/Files') or k.a.get('href').startswith('/cdp/'):
                 print('https://ca.kontur.ru'+k.a.get('href'))
+                cert_name = 'https://ca.kontur.ru'+k.a.get('href')
                 log_file('https://ca.kontur.ru'+k.a.get('href'), 'link.txt')
-                url = 'https://ca.kontur.ru'+k.a.get('href') # использовать переменную для формирования записи файлов
+                url = r'https://ca.kontur.ru'+k.a.get('href') # использовать переменную для формирования записи файлов
+                download_file(url, cert_name)
+                # name_file = re.search('\.(crt|crl|zip)$', k.a.get('href'))
+                # print (name_file)
                 # todo включить функцию по записи сертификатов в каталог
             else:
                 print(k.a.get('href'))
+                cert_name = k.a.get('href')
                 log_file(k.a.get('href'), 'link.txt')
-                url = k.a.get('href') # использовать переменную для формирования записи файлов
+                url = k.a.get('href') 
+                # использовать переменную для формирования записи файлов
+                download_file(url, cert_name)
